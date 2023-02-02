9d5576144a041fd5ce420d94a18eeffa854c2812
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2023-02-03 01:15:55 +0330

    Accounting.DataLayer is developed for primary uses

---
+++ Accounting.DataLayer/Services/CustomerRepository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accounting.DataLayer.Repositories;
+using Accounting.DataLayer.Context;
+using System.Data.Entity;
+
+namespace Accounting.DataLayer.Services
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private Accounting_DBEntities db;
+
+        public CustomerRepository(Accounting_DBEntities context)
+        {
+            db = context;
+        }
+
+        public List<Customers> GetAllCustomers()
+        {
+            return db.Customers.ToList();
+        }
+
+        public Customers GetCustomerById(int customerId)
+        {
+            return db.Customers.Find(customerId);
+        }
+
+        public bool InsertCustomer(Customers customer)
+        {
+            try
+            {
+                db.Customers.Add(customer);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }            
+        }
+
+        public bool UpdateCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State = EntityState.Deleted;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteCustomer(int customerId)
+        {
+            try
+            {
+                var customer = GetCustomerById(customerId);
+                DeleteCustomer(customer);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }        
+    }
+}
